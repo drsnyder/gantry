748885aaafa82401851240ab30f4994f6164e063
Damon Snyder <drsnyder@gmail.com,damon@huddler-inc.com>
2011-06-17 17:20:24 -0700

    Adding clagon to the deps and using that instead of contrib.command-line.

--- project.clj
+++ project.clj
@@ -1,9 +1,6 @@
 (defproject gantry "0.0.1-SNAPSHOT"
-  :description "Operations support in clojure"
+  :description "Operations support and deployment in clojure"
   :dependencies [[clojure "1.2.0"]
-                 [clojure-contrib "1.2.0"]]
+                 [clojure-contrib "1.2.0"]
+                 [clargon "1.0.0"]]
   :main gantry.main)
-  ;              [clj-ssh "0.2.0"]
-  ;               [crane "0.1.2-SNAPSHOT"]]
-  ;:repositories {"snapshots" "http://mvn.getwoven.com/repos/woven-public-snapshots"
-  ;               "releases" "http://mvn.getwoven.com/repos/woven-public-releases"})
--- src/gantry/main.clj
+++ src/gantry/main.clj
@@ -1,10 +1,11 @@
 (ns gantry.main
-  (:use clojure.contrib.command-line
-        clojure.contrib.str-utils
+  (:use clojure.contrib.str-utils
+        clojure.contrib.java-utils
         [clojure.contrib.condition :only [handler-case raise print-stack-trace *condition*]]
         gantry.core
         gantry.log
         gantry.run)
+  (:require [clargon.core :as c])
   (:gen-class))
 
 
@@ -14,9 +15,9 @@
     (with-config config (fun))))
 
 (defn resolve-targets [file hosts]
-  (if file
-    (load-file file)
-    (create-config (reduce #(add %1 %2) (create-resource) hosts))))
+  (if (not (empty? hosts))
+    (create-config (reduce #(add %1 %2) (create-resource) hosts))
+    (load-file file)))
 
 (defn perform-actions [config action-file actions]
   (do
@@ -26,38 +27,47 @@
            (recur (call (first as) c) (rest as))))))
     ;(doall (map #(call % config (:args config)) actions))))
 
+(defn file-exists [path]
+  (. (clojure.contrib.java-utils/file path) exists))
 
-(defn error-exit [msg]
+
+(defn print-error [msg & exit]
    (binding [*out* *err*]
      (do 
        (print msg)
-       (System/exit 1))))
+       (and exit (System/exit 1)))))
 
+(def gantry-options [(c/optional ["-H" "--hosts"      "The hosts to run the tasks on" :default []] #(vec (.split % ",")))
+                     (c/optional ["-p" "--port"       "The ssh port to connect to on the remote hosts" :default 22])
+                     (c/optional ["-k" "--ssh-key"    "The ssh key to use to connect to on the remote hosts" :default nil])
+                     (c/optional ["-f" "--gantryfile" "Load the tasks from this file" :default "gantryfile"])
+                     (c/optional ["-t" "--tasks"      "The tasks to run" :default []] #(vec (.split % ",")))
+                     (c/optional ["-c" "--config"     "Load the configuration / resources from this file" :default nil])
+                     (c/optional ["-s" "--args"       "Arguments to be passed down to the tasks. Takes the form key=value[,key=value]" :default {}] #(merge-settings {} %))])
+
+  
 
 ; to run java -jar gantry-0.0.1-SNAPSHOT-standalone.jar --hosts host1,host2
 ;
 (defn -main [& args]
-  (with-command-line args
-      "Gantry"
-      [[hosts          H  "The remote hosts" ""]
-       [port           p  "The ssh port of the remote hosts" 22]
-       [ssh-key        k  "SSH key file to use for authentication" nil]
-       [action-file    f  "The file to load actions from" "gantryfile"]
-       [config-file    c  "The file to load your configuration from" nil]
-       [arg-settings  s  "Add arguments to be passed down to the actions" nil]
-       actions]
-    (do 
-      
-      (handler-case :type
-                    (let [base (resolve-targets config-file (re-split #"," hosts))
-                          config (set-args base (merge-settings {:port port} arg-settings))]
-                      (perform-actions config action-file actions))
-                    (handle :remote-failed
-                            (do (println (str "remote failed: \n" (:message *condition*))) 
-                              (print-stack-trace *condition*) 
-                              (System/exit 1))))
+  (let [opts (apply c/clargon (cons args (flatten gantry-options)))]
 
+    (do
+      ;(println "Parsed opts: " opts)
 
-      (debug (str "done .."))
-
-      (System/exit 0))))
+      (if (or (file-exists (:gantryfile opts)) (not (empty? (:hosts opts))))
+        (do 
+          (handler-case :type
+                        (let [base (resolve-targets (:config opts) (:hosts opts))
+                              config (set-args base (merge {:port (:port opts) :ssh-key (:ssh-key opts)} (:args opts)))]
+                          (perform-actions config (:gantryfile opts) (:tasks opts)))
+                        (handle :remote-failed
+                                (do (print-error (str "remote failed: \n" (:message *condition*))) 
+                                  (print-stack-trace *condition*) 
+                                  (System/exit 1)))))
+        (do 
+          (print-error "Error, we need either -f|--gantryfile, a gantryfile or -H") 
+          (c/show-help (flatten (map #(% "" args) gantry-options)))))
+        
+        ; FIXME: why do we need this?
+        (System/exit 0))))
